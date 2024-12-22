912037b5d54a9e246e43a0a1b1baf5337ae52469
adambpp <adambpp@gmail.com>
2024-12-21 21:13:07 -0600

    super basic email sending works

--- Project/code_gen.py
+++ Project/code_gen.py
@@ -1,4 +1,5 @@
 import secrets as s
+import smtplib, ssl
 
 class CodeGenerator:
 
@@ -15,3 +16,24 @@ class CodeGenerator:
     '''
     def generate_code(self):
         return "".join(str(s.randbelow(9)) for i in range(0, 6))
+
+
+
+# testing email stuff
+port = 465 # for SSL
+smtp_server = "smtp.gmail.com"
+sender_email = ""
+receiver_email = ""
+password = ""
+message = """\
+Subject: Hi there
+
+This message is sent from Python
+"""
+
+# create a secure SSL context
+context = ssl.create_default_context()
+
+with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
+    server.login(sender_email, password)
+    server.sendmail(sender_email, receiver_email, message)
