402e8cf3380a3b7c0dc978921ce5df737cf711ba
adambpp <adambpp@gmail.com>
2024-12-22 18:51:59 -0600

    now can send emails with html (code is from a certain website that I will link in readme)

--- Project/code_gen.py
+++ Project/code_gen.py
@@ -1,5 +1,7 @@
 import secrets as s
 import smtplib, ssl
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
 
 class CodeGenerator:
 
@@ -18,22 +20,56 @@ class CodeGenerator:
         return "".join(str(s.randbelow(9)) for i in range(0, 6))
 
 
+class EmailOTP:
+    def __init__(self):
+        pass
 
-# testing email stuff
-port = 465 # for SSL
-smtp_server = "smtp.gmail.com"
-sender_email = ""
-receiver_email = ""
-password = ""
-message = """\
-Subject: Hi there
 
-This message is sent from Python
-"""
+    def generate_email(self):
+        # variables to easily change values
+        otp = CodeGenerator().generate_code()
+        sender_email = ""
+        receiver_email = ""
+        password = ""
 
-# create a secure SSL context
-context = ssl.create_default_context()
+        message = MIMEMultipart("alternative")
+        message["Subject"] = "multipart test"
+        message["From"] = sender_email
+        message["To"] = receiver_email
 
-with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
-    server.login(sender_email, password)
-    server.sendmail(sender_email, receiver_email, message)
+        text = """\
+        Hi,
+        How are you?
+        Real Python has many great tutorials:
+        www.realpython.com"""
+        html = """\
+        <html>
+          <body>
+            <p>Hi,<br>
+               How are you?<br>
+               <a href="http://www.realpython.com">Real Python</a> 
+               has many great tutorials.
+            </p>
+          </body>
+        </html>
+        """
+
+        # turn the messages into plain/html MIMEText objects
+        part1 = MIMEText(text, "plain")
+        part2 = MIMEText(html, "html")
+
+        # add HTML/plain-test parts to MIMEMultipart message
+        # email client tries to render the last part (html) first
+        message.attach(part1)
+        message.attach(part2)
+
+        # create a secure connection with server and send email
+        context = ssl.create_default_context()
+        with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context) as server:
+            server.login(sender_email, password)
+            server.sendmail(sender_email, receiver_email, message.as_string())
+
+
+
+test = EmailOTP()
+test.generate_email()
