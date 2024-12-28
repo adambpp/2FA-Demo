829fa8f5d5e792f150069c520a4699608c570b06
adambpp <adambpp@gmail.com>
2024-12-27 23:53:36 -0600

    Added screen after 2FA where the main program would be if it existed

--- Project/gui.py
+++ Project/gui.py
@@ -13,56 +13,70 @@ class Application(customtkinter.CTk):
         self.acc_database = AccountStorage()
 
 
-        # LOGIN SCREEN UI CODE
-        login_screen = customtkinter.CTkFrame(master=self, border_width=2, width=200)
-        login_screen.pack(pady=20, padx=10,ipadx=50, ipady=25, expand=True)
+        ### LOGIN SCREEN UI CODE ###
+        self.login_screen = customtkinter.CTkFrame(master=self, border_width=2, width=200)
+        self.login_screen.pack(pady=20, padx=10,ipadx=50, ipady=25, expand=True)
 
-        label = customtkinter.CTkLabel(master=login_screen, text="Login System", font=("Arial", 24))
-        label.pack(pady=12, padx=10)
+        self.label = customtkinter.CTkLabel(master=self.login_screen, text="Login System", font=("Arial", 24))
+        self.label.pack(pady=12, padx=10)
 
-        self.new_email = customtkinter.CTkEntry(master=login_screen, placeholder_text="E-mail")
+        self.new_email = customtkinter.CTkEntry(master=self.login_screen, placeholder_text="E-mail")
         self.new_email.pack(pady=12, padx=10)
 
-        self.new_password = customtkinter.CTkEntry(master=login_screen, placeholder_text="Password", show="*")
+        self.new_password = customtkinter.CTkEntry(master=self.login_screen, placeholder_text="Password", show="*")
         self.new_password.pack(pady=12, padx=10)
 
-        login_button = customtkinter.CTkButton(master=login_screen, text="Create Account", command=lambda: self.login_button_callback(login_screen, otp_screen))
-        login_button.pack(pady=12, padx=10, expand=True)
+        self.login_button = customtkinter.CTkButton(master=self.login_screen, text="Create Account", command=self.login_button_callback)
+        self.login_button.pack(pady=12, padx=10, expand=True)
 
 
 
-        # 2FA SCREEN UI CODE
-        otp_screen = customtkinter.CTkFrame(master=self, border_width=2)
+        ### 2FA SCREEN UI CODE ###
+        self.otp_screen = customtkinter.CTkFrame(master=self, border_width=2)
+
+        self.otp_info_label = customtkinter.CTkLabel(master=self.otp_screen, text="Please enter the 6-digit code that was sent to your email", font=("Arial", 20))
+        self.otp_info_label.pack(pady=12, padx=10)
+        # accessing a protected method of the inherited tkinter label so that I can get the text to wrap
+        self.otp_info_label._label.configure(wraplength=275)
 
         self.otp_entry = customtkinter.CTkEntry(
-            master=otp_screen, font=("Arial", 48), validate="key", validatecommand=(self.register(self.input_validation), "%P")
+            master=self.otp_screen, font=("Arial", 48), validate="key", validatecommand=(self.register(self.input_validation), "%P")
         )
-        self.otp_entry.pack(pady=20, ipadx=75, ipady=15)
+        self.otp_entry.pack(pady=5, ipadx=75, ipady=15)
         # modifying the internal tkinter entry widget in order to center the inputted text
         self.otp_entry._entry.configure(justify="center")
 
-        self.invalid_label = customtkinter.CTkLabel(master=otp_screen, text="", font=("Arial", 12), text_color="red")
+        self.invalid_label = customtkinter.CTkLabel(master=self.otp_screen, text="", font=("Arial", 12), text_color="red")
         self.invalid_label.pack(pady=0, padx=10)
 
-        otp_submit_button = customtkinter.CTkButton(master=otp_screen, text="Submit", command=self.otp_button_callback)
-        otp_submit_button.pack(pady=12, padx=10,ipadx=30,ipady=15, expand=True)
+        self.otp_submit_button = customtkinter.CTkButton(master=self.otp_screen, text="Submit", command=self.otp_button_callback)
+        self.otp_submit_button.pack(padx=10,ipadx=30,ipady=15, expand=True)
 
 
-    def login_button_callback(self, login_screen, otp_screen):
-        print("my button was clicked")
+        ### EXTRA IDK SCREEN FOR SUCCESSFUL 2FA ###
+        self.main_system_screen = customtkinter.CTkFrame(master=self, border_width=2)
+
+        self.welcome_label = customtkinter.CTkLabel(master=self.main_system_screen, text="Welcome!", font=("Arial", 20))
+        self.welcome_label.pack(pady=12, padx=10, fill="both", expand=True)
+
+
+    def login_button_callback(self):
         self.acc_database.add_account(self.new_email.get(), self.new_password.get())
         self.acc_database.send_otp_code(self.new_email.get(), self.new_password.get())
-        login_screen.pack_forget()
-        otp_screen.pack(pady=20, padx=10, ipadx=100, ipady=50, expand=True)
+        self.login_screen.pack_forget()
+        self.otp_screen.pack(pady=20, padx=10, ipadx=100, ipady=50, expand=True)
 
     def otp_button_callback(self):
         code_verification = self.acc_database.verify_otp_code(self.otp_entry.get())
         if code_verification:
             self.invalid_label.configure(text="")
+            self.otp_screen.pack_forget()
+            self.main_system_screen.pack(pady=20, padx=10, ipadx=100, ipady=50, fill="both", expand=True)
             print("SUCCESS")
         else:
             self.invalid_label.configure(text="Invalid Code")
             print("FAILURE")
+
 
     def input_validation(self, text_if_allowed):
         # case for when there is only one char left which is needed or else it can't be deleted
